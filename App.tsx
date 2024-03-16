8e3b9cf6c03b567bc0e9b8afd16421f8d7a4dd05
Kaan Küreli <kkureli@gmail.com>
2024-03-16 11:08:47 +0300

    refactor: provider moved to app file

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import AppNavigation from './src/navigation/navigation';
+import FavoritesProvider from './src/context/favoritesProvider';
 
 const App = () => {
-  return <AppNavigation />;
+  return (
+    <FavoritesProvider>
+      <AppNavigation />
+    </FavoritesProvider>
+  );
 };
 
 export default App;
--- src/navigation/navigation.tsx
+++ src/navigation/navigation.tsx
@@ -1,15 +1,12 @@
 import React, {FC} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import FavoritesProvider from '../context/favoritesProvider';
 import TabNavigation from './tabNavigation/tabNavigation';
 
 const AppNavigation: FC = () => {
   return (
-    <FavoritesProvider>
-      <NavigationContainer>
-        <TabNavigation />
-      </NavigationContainer>
-    </FavoritesProvider>
+    <NavigationContainer>
+      <TabNavigation />
+    </NavigationContainer>
   );
 };
 
